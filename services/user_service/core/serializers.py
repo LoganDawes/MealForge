c5656a2e57ef00fceb3b4456e2220653db390c8f
LoganDawes <logantdawes@gmail.com>
2025-03-20 14:32:39 -0500

    Added user preferences field validation

--- services/user_service/core/serializers.py
+++ services/user_service/core/serializers.py
@@ -20,9 +20,47 @@ class UserSerializer(serializers.ModelSerializer):
     
 
 class UserPreferencesSerializer(serializers.ModelSerializer):
+    DIET_CHOICES = [
+        ("Gluten free", "Gluten Free"),
+        ("Ketogenic", "Ketogenic"),
+        ("Vegetarian", "Vegetarian"),
+        ("Lacto-vegetarian", "Lacto-Vegetarian"),
+        ("Ovo-vegetarian", "Ovo-Vegetarian"),
+        ("Vegan", "Vegan"),
+        ("Pescetarian", "Pescetarian"),
+        ("Paleo", "Paleo"),
+        ("Primal", "Primal"),
+        ("Low fodmap", "Low FODMAP"),
+        ("Whole30", "Whole30"),
+    ]
+
+    INTOLERANCE_CHOICES = [
+        ("Dairy", "Dairy"),
+        ("Egg", "Egg"),
+        ("Gluten", "Gluten"),
+        ("Grain", "Grain"),
+        ("Peanut", "Peanut"),
+        ("Seafood", "Seafood"),
+        ("Sesame", "Sesame"),
+        ("Shellfish", "Shellfish"),
+        ("Soy", "Soy"),
+        ("Sulfite", "Sulfite"),
+        ("Tree nut", "Tree Nut"),
+        ("Wheat", "Wheat"),
+    ]
+
+    diets = serializers.MultipleChoiceField(choices=DIET_CHOICES)
+    intolerances = serializers.MultipleChoiceField(choices=INTOLERANCE_CHOICES)
+
     class Meta:
         model = UserPreferences
         fields = ["language", "diets", "intolerances", "calorie_limit"]
+
+    def validate_diets(self, value):
+        return list(value)
+
+    def validate_intolerances(self, value):
+        return list(value)
 
     # Method to validate the calorie limit
     def validate_calorie_limit(self, value):
