c0f2f1a85547c45a59a85306932c9d266449e2bc
LoganDawes <logantdawes@gmail.com>
2025-03-25 15:29:56 -0500

    Installed frontend files

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to MealForge</h1>
+      {/* Add your components and logic here */}
+    </div>
+  );
+}
+
+export default App;
